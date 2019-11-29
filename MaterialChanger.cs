0aa213f519580500d1841dfff2c1af8ba7d02d6e
Kim Dotcom <kim.dotcom@email.cz>
2019-11-29 18:23:50 +0100

    added materialChanger and sceneLoader

---
+++ MaterialChanger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+    [System.Serializable]
+    public struct MaterialChangerData
+    {
+        public Material material1;
+        public Material material2;
+        public KeyCode switchKey;
+        public GameObject switchableObject;
+        [HideInInspector]
+        public MeshRenderer objectRenderer;
+        [HideInInspector]
+        public bool isSwitched;
+    }
+    public MaterialChangerData[] changerList;
+
+    //init first material on start
+    void Start()
+    {
+        for (int i = 0; i < changerList.Length; i++)
+        {
+            changerList[i].isSwitched = false;
+            changerList[i].objectRenderer = changerList[i].switchableObject.GetComponent<MeshRenderer>();
+            changerList[i].objectRenderer.material = changerList[i].material1;
+        }
+    }
+
+    //switch materials on keypress
+    void Update()
+    {
+        for (int i = 0; i < changerList.Length; i++)
+        {
+            if (Input.GetKeyDown(changerList[i].switchKey))
+            {
+                if (!changerList[i].isSwitched) {
+                    changerList[i].objectRenderer.material = changerList[i].material2;
+                }
+                else
+                {
+                    changerList[i].objectRenderer.material = changerList[i].material1;
+                }
+                changerList[i].isSwitched = !changerList[i].isSwitched;
+            }
+        }
+    }
+}
---
+++ SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [System.Serializable]
+    public struct SceneList
+    {
+        public KeyCode sceneSwitchKey;
+        public string sceneName;
+    }
+    //for seamless scene switching, a preset of scenes has to be:
+        //included in build settings
+        //copied and pasted into all the scenes with the same key/scene mappings
+    public SceneList[] sceneSet;
+
+    void Update()
+    {
+        foreach (SceneList thisScene in sceneSet)
+        {
+            if (Input.GetKeyDown(thisScene.sceneSwitchKey))
+            {
+                if (thisScene.sceneName != null) {
+                    Debug.Log("Switching to scene " + thisScene.sceneName);
+                    SceneManager.LoadScene(thisScene.sceneName);
+                }
+            }
+        }
+    }
+}
