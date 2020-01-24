f55ae4bb2ce27bcd7e933477a1d55fef6c7eb744
Kim Dotcom <kim.dotcom@email.cz>
2020-01-24 14:31:21 +0100

    sceneLoader. again

---
+++ SceneLoader2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader2 : MonoBehaviour
+{
+	//unique keycode
+    public List<KeyCode> loadKeys;
+    //unique scene name
+		//has to be specified without the ".unity" suffix
+		//has to be also added to the build setting / scene order
+    public List<string> loadScenes;
+
+    void Update()
+    {
+        foreach (KeyCode key in loadKeys)
+        {
+            if (Input.GetKeyDown(loadKeys[loadKeys.IndexOf(key)]) && loadScenes[loadKeys.IndexOf(key)] != null) {
+                Application.LoadLevel(loadScenes[loadKeys.IndexOf(key)]);
+            }
+        }
+    }
+}
